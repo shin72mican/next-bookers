135d4f46861d57627f962220c5b0fc39e4233d7b
root <root@DESKTOP-0UKCNUK.>
2025-02-22 18:01:27 +0900

    deploy fix

--- packages/front-app/src/app/_components/users/UserList/index.tsx
+++ packages/front-app/src/app/_components/users/UserList/index.tsx
@@ -1,10 +1,10 @@
 // import Link from 'next/link';
-import { getUserList } from '@/services/users/getUserList';
-import { UserCollection } from '@/app/lib/class/userCollection';
-import { User } from '@/app/lib/class/user';
+// import { getUserList } from '@/services/users/getUserList';
+// import { UserCollection } from '@/app/lib/class/userCollection';
+// import { User } from '@/app/lib/class/user';
 
 export const UserList = async() => {
-  const userCollection:UserCollection = await getUserList();
+  // const userCollection:UserCollection = await getUserList();
   return (
     <>
       <table>
